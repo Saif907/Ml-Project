9c29cc0f5bcc1dc988a968d2cbb661853cede7a2
Saif907 <saif81868@gmail.com>
2025-09-21 00:51:07 +0530

    data ingestion with transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,14 +5,20 @@ from src.exception import CustomException
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
+    """ This class is responsible for data ingestion configuration
+    """
     train_data_path: str = os.path.join('artifacts', 'train.csv')
     test_data_path: str = os.path.join('artifacts', 'test.csv')
     raw_data_path: str = os.path.join('artifacts', 'data.csv')
 
 class DataIngestion:
+    """ This class is responsible for data ingestion
+    """
     def __init__(self):
         self.ingestion_config = DataIngestionConfig()
     
@@ -41,5 +47,9 @@ class DataIngestion:
             raise CustomException(e, sys)
         
 if __name__ == "__main__":
+    "This is to test the data ingestion module"
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
